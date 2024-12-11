12fb9c47dc9d5ea6b616eed70a2de17dfc606647
Kushal Azim Ekram <mekram@microsoft.com>
2024-12-10 19:42:21 -0800

    feat: Test file creation update.

--- create-test-files.sh
+++ create-test-files.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
-# this is a sorry excuse for a test, but hey, it kinda works
 
 #tmpdir=$(mktemp -d)
 tmpdir=$1;
 [ -z "$tmpdir" ] && tmpdir=temp
 
-subdir_name="subdir"
-subdir_name2="subdir-2"
-subdir_dot=".dot-subdir"
-subsubdir_name="subsubdir"
-dirs_created=("$tmpdir" "$tmpdir/$subdir_name" "$tmpdir/$subdir_dot" "$tmpdir/$subdir_name2" "$tmpdir/$subdir_name/$subsubdir_name" )
+test_dirs=(
+  "$tmpdir" "$tmpdir/subdir" "$tmpdir/.dot-subdir" "$tmpdir/.dot-subdir with space" "$tmpdir/subdir/subsubdir" "$tmpdir/subdir with space" "$tmpdir/subdir/subsubdir with space" "$tmpdir/subdir with space" "$tmpdir/subdir with space/subsubdir with space"  
+  # "$tmpdir" "$tmpdir/subdir" "$tmpdir/subdir2" "$tmpdir/subdir3" "$tmpdir/.dot-subdir" "$tmpdir/subdir/subsubdir" "$tmpdir/subdir/subsubdir2" "$tmpdir/subdir/subsubdir3" "$tmpdir/subdir/subsubdir4" "$tmpdir/subdir with space"
+)
 
 files=(
-"Line1\r\nLine2;;crlf-only"
-"Line1\r\nLine2\nLine3;;crlf-with-lf-mixed"
-"Line1\nLine2;;lf-only"
-"Line1\r\nLine2;;exclude-crlf"
-"Line1\nLine2;;exclude-lf"
-"Line1\r\nLine2\nLine3;;exclude-mixed")
+# 8 CRLF only files
+"Line1\r\nLine2\r\nLine3\r\n;;crlf-only"
+"Line1\r\nLine2\r\nLine3\r\n;;crlf-only with space"
+"Line1\r\nLine2\r\nLine3\r\n;;CRLF-onlyCAP"
+"Line1\r\nLine2\r\nLine3\r\n;;CRLF-only CAP with space"
 
-for d in "${dirs_created[@]}"; do
+"Line1\r\nLine2\r\nLine3;;exclude-crlf-only-regex-test"
+"Line1\r\nLine2\r\nLine3;;exclude-crlf-only-regex-test with space"
+"Line1\r\nLine2\r\nLine3;;EXCLUDE-CRLF-only-regex-testCAP"
+"Line1\r\nLine2\r\nLine3;;EXCLUDE-CRLF-only-regex-test with space CAP"
+
+# 8 CRLF with LF mixed files
+"Line1\r\nLine2\nLine3\nLine4\r\n;;crlf-with-lf-mixed"
+"Line1\r\nLine2\nLine3\nLine4\r\n;;crlf-with-lf-mixed with space"
+"Line1\r\nLine2\nLine3\nLine4\r\n;;CRLF-with-LF-mixedCAP"
+"Line1\r\nLine2\nLine3\nLine4\r\n;;CRLF-with-LF-mixed CAP with space"
+
+"Line1\r\nLine2\nLine3\r\nLine4\r\n;;exclude-crlf-with-lf-mixed-regex-test"
+"Line1\r\nLine2\nLine3\r\nLine4\r\n;;exclude-crlf-with-lf-mixed-regex-test with space"
+"Line1\r\nLine2\nLine3\r\nLine4\r\n;;EXCLUDE-CRLF-with-LF-mixed-regex-testCAP"
+"Line1\r\nLine2\nLine3\r\nLine4\r\n;;EXCLUDE-CRLF-with-LF-mixed-regex-test CAP with space"
+
+# 8 LF only files
+"Line1\nLine2\nLine3\n;;lf-only"
+"Line1\nLine2\nLine3\n;;lf-only with space"
+"Line1\nLine2\nLine3\n;;LF-onlyCAP"
+"Line1\nLine2\nLine3\n;;LF-only CAP with space"
+
+"Line1\nLine2\nLine2\n;;exclude-lf-only-regex-test"
+"Line1\nLine2\nLine2\n;;exclude-lf-only-regex-test with space"
+"Line1\nLine2\nLine2\n;;EXCLUDE-LF-only-regex-testCAP"
+"Line1\nLine2\nLine2\n;;EXCLUDE-LF-only-regex-test CAP with space"
+
+)
+
+file_count=$(( ${#files[@]} * ${#test_dirs[@]} ))
+echo "Creating $file_count files in ${#test_dirs[@]} directories">&2
+for d in "${test_dirs[@]}"; do
     mkdir -p "$d"
     for f in "${files[@]}"; do
         c=$(echo "$f" | awk -F';;' '{print $1}')
