fc0ab89c5fffa889c1c2e5b89f23c836a3e7034e
Kushal Azim Ekram <mekram@microsoft.com>
2025-01-07 01:25:44 -0800

    feat: Added pattern type option. Now we can choose between shell_glob filename search and regex pattern type

--- create-test-files.bash
+++ create-test-files.bash
@@ -13,35 +13,35 @@ files=(
 # 8 CRLF only files
 "Line1\r\nLine2\r\nLine3\r\n;;crlf-only"
 "Line1\r\nLine2\r\nLine3\r\n;;crlf-only with space"
-"Line1\r\nLine2\r\nLine3\r\n;;CRLF-onlyCAP"
-"Line1\r\nLine2\r\nLine3\r\n;;CRLF-only CAP with space"
+"Line1\r\nLine2\r\nLine3\r\n;;CRLF-only"
+"Line1\r\nLine2\r\nLine3\r\n;;CRLF-only with space"
 
-"Line1\r\nLine2\r\nLine3;;exclude-crlf-only-regex-test"
-"Line1\r\nLine2\r\nLine3;;exclude-crlf-only-regex-test with space"
-"Line1\r\nLine2\r\nLine3;;EXCLUDE-CRLF-only-regex-testCAP"
-"Line1\r\nLine2\r\nLine3;;EXCLUDE-CRLF-only-regex-test with space CAP"
+"Line1\r\nLine2\r\nLine3;;exclude-crlf-only"
+"Line1\r\nLine2\r\nLine3;;exclude-crlf-only with space"
+"Line1\r\nLine2\r\nLine3;;EXCLUDE-CRLF-only"
+"Line1\r\nLine2\r\nLine3;;EXCLUDE-CRLF-only with space"
 
 # 8 CRLF with LF mixed files
 "Line1\r\nLine2\nLine3\nLine4\r\n;;crlf-with-lf-mixed"
 "Line1\r\nLine2\nLine3\nLine4\r\n;;crlf-with-lf-mixed with space"
-"Line1\r\nLine2\nLine3\nLine4\r\n;;CRLF-with-LF-mixedCAP"
-"Line1\r\nLine2\nLine3\nLine4\r\n;;CRLF-with-LF-mixed CAP with space"
+"Line1\r\nLine2\nLine3\nLine4\r\n;;CRLF-with-LF-mixed"
+"Line1\r\nLine2\nLine3\nLine4\r\n;;CRLF-with-LF-mixed with space"
 
-"Line1\r\nLine2\nLine3\r\nLine4\r\n;;exclude-crlf-with-lf-mixed-regex-test"
-"Line1\r\nLine2\nLine3\r\nLine4\r\n;;exclude-crlf-with-lf-mixed-regex-test with space"
-"Line1\r\nLine2\nLine3\r\nLine4\r\n;;EXCLUDE-CRLF-with-LF-mixed-regex-testCAP"
-"Line1\r\nLine2\nLine3\r\nLine4\r\n;;EXCLUDE-CRLF-with-LF-mixed-regex-test CAP with space"
+"Line1\r\nLine2\nLine3\r\nLine4\r\n;;exclude-crlf-with-lf-mixed"
+"Line1\r\nLine2\nLine3\r\nLine4\r\n;;exclude-crlf-with-lf-mixed with space"
+"Line1\r\nLine2\nLine3\r\nLine4\r\n;;EXCLUDE-CRLF-with-LF-mixed"
+"Line1\r\nLine2\nLine3\r\nLine4\r\n;;EXCLUDE-CRLF-with-LF-mixed with space"
 
 # 8 LF only files
 "Line1\nLine2\nLine3\n;;lf-only"
 "Line1\nLine2\nLine3\n;;lf-only with space"
-"Line1\nLine2\nLine3\n;;LF-onlyCAP"
-"Line1\nLine2\nLine3\n;;LF-only CAP with space"
+"Line1\nLine2\nLine3\n;;LF-only"
+"Line1\nLine2\nLine3\n;;LF-only with space"
 
-"Line1\nLine2\nLine2\n;;exclude-lf-only-regex-test"
-"Line1\nLine2\nLine2\n;;exclude-lf-only-regex-test with space"
-"Line1\nLine2\nLine2\n;;EXCLUDE-LF-only-regex-testCAP"
-"Line1\nLine2\nLine2\n;;EXCLUDE-LF-only-regex-test CAP with space"
+"Line1\nLine2\nLine2\n;;exclude-lf-only"
+"Line1\nLine2\nLine2\n;;exclude-lf-only with space"
+"Line1\nLine2\nLine2\n;;EXCLUDE-LF-only"
+"Line1\nLine2\nLine2\n;;EXCLUDE-LF-only with space"
 
 )
 
--- entrypoint.bash
+++ entrypoint.bash
@@ -36,8 +36,8 @@ usage() {
                      e.g. if you want to include files only in the 'subdir', use '^./target-dir/subdir/.*'
     -l  LINE_ENDING  Line Ending type [default: 'CRLF'].
                      Valid values comes from 'file' cmd (CRLF, LF, or MIXED).
-    -i  INCL_REGEX   Regex to include path(s) [default '.*'].
-    -e  EXCL_REGEX   Regex to exclude path(s) [default:: '^$'].
+    -i  INCL_PATTERN Regex to include path(s) [default '.*'].
+    -e  EXCL_PATTERN Regex to exclude path(s) [default:: '^$'].
     -d  DIR_DEPTH    Directory depth to search [default: 999].
     -h  --help       Show this help message.
     
@@ -51,16 +51,18 @@ usage() {
 while [ $# -gt 0 ]; do
     opt="$1" value="$2"
     case "$opt" in
-        -t)
+        -t|--target-dir)
         target_dir="$value";shift 1;;
-        -l)
+        -l|--line-ending)
         line_ending_type="$value";shift 1;;
-        -i)
-        include_regex="$value";shift 1;;
-        -e)
-        exclude_regex="$value";shift 1;;
-        -d)
+        -i|--include)
+        include_pattern="$value";shift 1;;
+        -e|--exclude)
+        exclude_pattern="$value";shift 1;;
+        -d|--dir-depth)
         max_dir_depth="$value";shift 1;;
+        -p|--pattern-type)
+        pattern_type="$value";shift 1;;
         -h|--help)
             usage;exit 0;;
         *)
@@ -75,20 +77,42 @@ target_dir="${target_dir:-"."}"  # Default is current directory
 # [[ -z $folder ]] && folder="." # Default is current directory
 line_ending_type="${line_ending_type:-"crlf"}"  # Default is CRLF
 line_ending_type="$(tr '[:upper:]' '[:lower:]' <<< "$line_ending_type")"
-include_regex="${include_regex:-".*"}"  # Default is include all
-exclude_regex="${exclude_regex:-"^$"}"  # Default exclude none
+pattern_type="${pattern_type:-"shell_glob"}"  # Default exclude none
 max_dir_depth=${max_dir_depth:-999}
 
+if [[ "$pattern_type" == "shell_glob" ]]; then
+  WARNING "Only filenames will be used with shell_glob pattern type for include/exclude pattern, so craft your glob pattern for filenames only"
+  include_pattern="${include_pattern:-"*"}"  # Default is include all
+  exclude_pattern="${exclude_pattern:-""}"  # Default exclude none
+elif [[ "$pattern_type" == "regex" ]]; then
+  WARNING "'$target_dir' will be included in the path while applying regex, so craft your regex accordingly"
+  include_pattern="${include_pattern:-".*"}"  # Default is include all
+  exclude_pattern="${exclude_pattern:-"^$"}"  # Default exclude none
+else
+  ERROR "Invalid pattern type ($pattern_type). Possible types are 'shell_glob' or 'regex'"
+  exit 1
+fi
+
 INFO "Line Ending Type: '$line_ending_type'"
-INFO "Include regex   : '$include_regex'"
-INFO "Exclude regex   : '$exclude_regex'"
+INFO "Include regex   : '$include_pattern'"
+INFO "Exclude regex   : '$exclude_pattern'"
 INFO "Max dir depth   : '$max_dir_depth'"
-INFO "Target directory: $target_dir"
-WARNING "'$target_dir' will be included in the path, so craft your regex accordingly"
+INFO "Target directory: '$target_dir'"
 # log_info "Checking '$line_ending_type' endings in directory: $target_dir"
 
-# declare -a files_array 
-mapfile -d '' files_array < <(find "$target_dir" -maxdepth "$max_dir_depth" -not -path "*/.git/*" -type f -regex "$include_regex" -not -regex "$exclude_regex" -print0 | xargs -r0 file)
+# Yes yes, this is a bit ugly and duplicate if-else
+# but I wanted to organize and print incl/excl info before mapfile,
+# declare -a files_array
+if [[ "$pattern_type" == "shell_glob" ]]; then
+  mapfile -d '' files_array < <(find "$target_dir" -maxdepth "$max_dir_depth" -not -path "*/.git/*" -type f -name "$include_pattern" -not -name "$exclude_pattern" -print0 | xargs -r0 file)
+elif [[ "$pattern_type" == "regex" ]]; then
+  mapfile -d '' files_array < <(find "$target_dir" -maxdepth "$max_dir_depth" -not -path "*/.git/*" -type f -regextype egrep -regex "$include_pattern" -not -regex "$exclude_pattern" -print0 | xargs -r0 file)
+else
+  ERROR "Invalid pattern type ($pattern_type). Possible types are 'shell_glob' or 'regex'"
+  exit 1
+fi
+
+# mapfile -d '' files_array < <(find "$target_dir" -maxdepth "$max_dir_depth" -not -path "*/.git/*" -type f -regextype egrep -regex "$include_pattern" -not -regex "$exclude_pattern" -print0 | xargs -r0 file)
 
 # echo "${files_array[@]}"
 case "$line_ending_type" in
@@ -126,12 +150,14 @@ else
       echo "EOF"
     } >> "$GITHUB_ENV"
   fi
-      echo "FILE_COUNT=${count_of_file}" >> "$GITHUB_OUTPUT"
+  if [[ -n "$GITHUB_OUTPUT" ]]; then
+    echo "FILE_COUNT=${count_of_file}" >> "$GITHUB_OUTPUT"
     {
       echo "FILE_LIST<<EOF"
       echo "${found_files[*]}"
       echo "EOF"
     } >> "$GITHUB_OUTPUT"
+  fi
   exit 1
 fi
 
